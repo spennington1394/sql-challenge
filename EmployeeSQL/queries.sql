cba39f869de7057c629d8e63d681d3440f328580
spennington <spennington13@gmail.com>
2020-11-19 15:15:46 -0500

    done
    done hw... BONUS?

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -65,3 +65,11 @@ on dept_emp.dept_no = departments.dept_no
 left join employees 
 on dept_emp.emp_no = employees.emp_no
 where dept_name = 'Development' or dept_name = 'Sales'
+
+--In descending order, list the frequency count of employee last names,
+--i.e., how many employees share each last name
+
+select last_name, count(last_name) as "Last Name Count"
+from employees
+group by last_name
+order by "Last Name Count" desc
