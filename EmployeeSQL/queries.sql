8f68f1bfd0f00248cda665b6fd7b432dd037b383
spennington <spennington13@gmail.com>
2020-11-19 14:54:19 -0500

    #5 done
    5 done

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -38,3 +38,9 @@ left join departments
 on dept_emp.dept_no = departments.dept_no
 left join employees 
 on dept_emp.emp_no = employees.emp_no
+
+--List first name, last name, and sex for employees whose 
+--first name is "Hercules" and last names begin with "B."
+select first_name, last_name, sex
+from employees
+where first_name = 'Hercules' and last_name like 'B%' 
