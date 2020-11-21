c009f2d6af377d5660e2a8e30f9610a0e2fc2277
spennington <spennington13@gmail.com>
2020-11-20 20:31:18 -0500

    sql-challenge
    sql-challenge

--- EmployeeSQL/Create tables.sql
+++ EmployeeSQL/Create tables.sql
@@ -10,38 +10,39 @@ create table Employees(
 	sex varchar not null,
 	hire_date date not null,
 	primary key (emp_no),
---	foreign key (emp_title_id) references Titles (title_id)
+	foreign key (emp_title_id) references Titles (title_id)
 );
 
 create table Salaries(
 	emp_no varchar not null,
 	salary integer not null,
+	primary key (emp_no)
 	foreign key (emp_no) references Employees (emp_no)
 );
 
 create table Dept_Emp(
 	emp_no varchar not null,
 	dept_no varchar not null,
+	primary key (dept_no, emp_no),
 	foreign key (emp_no) references Employees (emp_no)
 );
 
 create table Dept_Manager(
 	dept_no varchar not null,
 	emp_no varchar not null,
---error when importing title_id already exists as a key
---	primary key (dept_no),
+	primary key (emp_no, dept_no),
 	foreign key (emp_no) references Employees (emp_no)
 );
 
 create table Titles(
 	title_id varchar not null,
 	title varchar not null,
---error when importing title_id already exists as a key
---	primary key (title_id)
+	primary key (title_id)
 );
 
 create table Departments(
 	dept_no varchar not null,
 	dept_name varchar not null,
+	primary key(dept_no),
 	foreign key (dept_no) references Dept_Manager(dept_no)
 );
